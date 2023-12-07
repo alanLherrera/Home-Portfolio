cd18393474ca098d4c470f1eb4c6d5173f9ff4dc
alanLherrera <alanherrera06@gmail.com>
2023-12-07 11:33:00 -0800

    lot update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+
 import {
   About,
   Contact,
@@ -14,7 +15,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div classname="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
